f9edf5e67d53946cfc07df3d0bf524b3eedcb077
Malov Victor <malvikus@gmail.com>
2018-08-08 13:58:12 +0300

    Add projects page

--- server.js
+++ server.js
@@ -38,12 +38,19 @@ app.get('/', (req, res) => {
     res.render('home.hbs', {
         pageTitle: 'Home Page',
         welcomeMessage: 'Welcome to my website'
-    })
+    });
 });
 
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
         pageTitle: 'About Page'
+    });
+});
+
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Page',
+        welcomeMessage: 'Look at my projects'
     });
 });
 
